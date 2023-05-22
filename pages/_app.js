fbc2812e9e13c8ad9bd82c9cd9ea5877259110a3
Sirbu Gabriel <sirbugabriel97@gmail.com>
2023-05-22 18:27:52 +0300

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 }
